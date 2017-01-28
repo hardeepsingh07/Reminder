3b2d0a6cbd6e1252c1f29eea3bf89323820f8e36
Kevin Ali <kevinali50@yahoo.com>
2017-01-28 13:41:59 -0800

    A3/Security features added.

---
+++ src/main/java/edu/cpp/cs580/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package edu.cpp.cs580;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            //This will list what links are not going to be authenticate so we will have to put in the registration page which i used as sample
+                .antMatchers("/success","/cs580/ping").permitAll()
+                .anyRequest().authenticated()
+                .and()
+             //This is the login page i think we need to change the way to do login, 
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user@cpp.edu").password("password").roles("USER");
+    }
+}
--- src/main/resources/templates/home.ftl
+++ src/main/resources/templates/home.ftl
@@ -96,7 +96,8 @@
                 <label>Username: </label><input type="text" id="rUserName">
                         <button onclick="validateInput()">Validate</button>
                 <td><label id="checkResult"></label></td>
-            </div>  
+            </div>
+  
         </div>
 
 
