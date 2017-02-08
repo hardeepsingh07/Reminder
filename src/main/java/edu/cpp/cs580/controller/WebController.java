dd17f101c83b3c544ae3972c290c9cb8494425a7
Hardeep Singh <hardeep07@yahoo.com>
2017-02-08 14:40:53 -0800

    Code Cleanup

--- src/main/java/edu/cpp/cs580/controller/WebController.java
+++ src/main/java/edu/cpp/cs580/controller/WebController.java
@@ -235,19 +235,19 @@ public class WebController {
     }
     
     @RequestMapping(value = "/log/{logString}", method = RequestMethod.GET)
-    String log(@PathVariable("logString") String logString) {
+    String logger(@PathVariable("logString") String logString) {
         Logger.debug(logString);
         return "Succesfully Logged " + logString;
     }
 
     @RequestMapping(value = "/login")
-    ModelAndView test() {
+    ModelAndView logIn() {
         ModelAndView modelAndView = new ModelAndView("login");
         return modelAndView;
     }
 
     @RequestMapping(value = "/success")
-    ModelAndView GG() {
+    ModelAndView success() {
         ModelAndView modelAndView = new ModelAndView("success");
         return modelAndView;
     }
@@ -263,31 +263,6 @@ public class WebController {
     ModelAndView forgotPasswordMV() {
         ModelAndView modelAndView = new ModelAndView("forgotPassword");
         return modelAndView;
-    }
-
-
-    @RequestMapping(value = "/hyperlinks", method = RequestMethod.GET)
-    String hyper() {
-        StringBuffer html = new StringBuffer();
-
-        html.append("<!DOCTYPE html>");
-        html.append("<html lang=\"en\">");
-        html.append("<head>");
-        html.append("<meta charset=\"UTF-8\" />");
-        html.append("<title>Hollywood Life</title>");
-        html.append("<meta name=\"description\" content=\"The latest entertainment news\" />");
-        html.append("<meta name=\"keywords\" content=\"hollywood gossip, hollywood news\" />");
-        html.append("</head>");
-        html.append("<body>");
-        html.append("<div id='color'>This is red</div> />");
-        html.append("</body>");
-        html.append("</html>");
-
-        Document doc = Jsoup.parse(html.toString());
-
-        //get meta description content
-        String description = doc.select("meta[name=description]").get(0).attr("content");
-        return " This is a demo of how to get Meta description : " + description;
     }
 }
 
